91b5a5053c08d4559ad2e42b9dea131615c15fbc
cappuccinocat <xinglydia@gmail.com>
2018-08-10 16:51:13 -0400

    fix redirect on wrong password bug

--- app.js
+++ app.js
@@ -14,28 +14,20 @@ var models = require('./models/models');
 
 //setup passport
 passport.use(new LocalStrategy(function(username, password, done) {
+  if(!username || !password) {
+    return done(null, false);
+  }
+  
   models.User.findOne({
     username: username
   }, function(err, user) {
-    // if there's an error, finish trying to authenticate (auth failed)
-    if (err) {
-      console.log(err);
-      return done(err);
-    }
-    // if no user present, auth failed
-    if (!user) {
-      console.log(user);
-      return done(null, false);
-    }
-
-    // if passwords do not match, auth failed
     bcrypt.compare(password, user.password, function(err, res) {
-      if (!res) {
+      if (res) {
+        return done(null, user);
+      } else {
         return done(null, false);
       }
     });
-    // auth has has succeeded
-    return done(null, user);
   });
 }));
 
